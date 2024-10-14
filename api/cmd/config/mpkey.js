dbdd506a7d6786f1b01074faee338e6ea4d8237d
xiaochenxi111 <2140492217@qq.com>
2024-10-14 17:40:05 +0800

    Add files via upload

---
+++ api/cmd/config/mpkey.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/**
+ * 小程序配置appid和secret
+ * 可以通过环境变量或直接配置
+ */
+module.exports = {
+  appid: process.env.MP_APPID || 'wx3c3d5b34eb6c1e50',
+  secret: process.env.MP_SECRET || 'bdb2783e7f40d6f05c1353206bcf757b'
+}
